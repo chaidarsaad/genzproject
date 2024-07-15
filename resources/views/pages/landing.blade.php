3ae8a8915d7e81780626355287f8cf7ffd981956
Chaidar Saad <chaidarsaad55@gmail.com>
2024-07-15 10:09:25 +0700

    Update landing.blade.php

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -110,42 +110,46 @@
             <div class="container xl:max-w-6xl mx-auto px-4">
                 <div class="flex flex-wrap flex-row -mx-4 justify-center">
                     <!-- content -->
-                    <div class="flex-shrink max-w-full px-4 sm:px-12 lg:px-18 w-full sm:w-9/12 lg:w-1/2 self-center">
-                        <img src="{{ Storage::url($hero->image) }}" class="w-full max-w-full h-auto" alt="hero image">
-                    </div><!-- end content -->
+                    @if ($hero != null)
+                        <div
+                            class="flex-shrink max-w-full px-4 sm:px-12 lg:px-18 w-full sm:w-9/12 lg:w-1/2 self-center">
+                            <img src="{{ Storage::url($hero->image) }}" class="w-full max-w-full h-auto"
+                                alt="hero image">
+                        </div><!-- end content -->
 
-                    <!-- text -->
-                    <div class="flex-shrink max-w-full px-4 w-full md:w-9/12 lg:w-1/2 self-center lg:pr-12">
-                        <div class="text-center lg:text-left mt-6 lg:mt-0">
-                            <div class="mb-12">
-                                <h1 class="text-3xl leading-normal text-black font-bold mb-4">
-                                    {{ $hero->title }}<br>
-                                    <span data-toggle="typed"
-                                        data-options='{"strings": {{ $hero->services = json_encode($hero->services) }} }'></span>
-                                </h1>
-                                <p class="text-gray-500 leading-relaxed font-light text-xl mx-auto pb-2">
-                                    {{ $hero->subtitle }}</p>
+                        <!-- text -->
+                        <div class="flex-shrink max-w-full px-4 w-full md:w-9/12 lg:w-1/2 self-center lg:pr-12">
+                            <div class="text-center lg:text-left mt-6 lg:mt-0">
+                                <div class="mb-12">
+                                    <h1 class="text-3xl leading-normal text-black font-bold mb-4">
+                                        {{ $hero->title }}<br>
+                                        <span data-toggle="typed"
+                                            data-options='{"strings": {{ $hero->services = json_encode($hero->services) }} }'></span>
+                                    </h1>
+                                    <p class="text-gray-500 leading-relaxed font-light text-xl mx-auto pb-2">
+                                        {{ $hero->subtitle }}</p>
+                                </div>
+                                <a class="py-2.5 px-10 inline-block text-center leading-normal text-gray-900 bg-white border-b border-gray-100 hover:text-black hover:ring-0 focus:outline-none focus:ring-0 mr-4"
+                                    href="#portfolio">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="inline-block mr-1" width="1.5rem"
+                                        height="1.5rem" fill="currentColor" viewBox="0 0 512 512">
+                                        <path
+                                            d="M304,384V360c0-29,31.54-56.43,52-76,28.84-27.57,44-64.61,44-108,0-80-63.73-144-144-144A143.6,143.6,0,0,0,112,176c0,41.84,15.81,81.39,44,108,20.35,19.21,52,46.7,52,76v24"
+                                            style="fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" />
+                                        <line x1="224" y1="480" x2="288" y2="480"
+                                            style="fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" />
+                                        <line x1="208" y1="432" x2="304" y2="432"
+                                            style="fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" />
+                                        <line x1="256" y1="384" x2="256" y2="256"
+                                            style="fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" />
+                                        <path d="M294,240s-21.51,16-38,16-38-16-38-16"
+                                            style="fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" />
+                                    </svg>
+                                    Our Work
+                                </a>
                             </div>
-                            <a class="py-2.5 px-10 inline-block text-center leading-normal text-gray-900 bg-white border-b border-gray-100 hover:text-black hover:ring-0 focus:outline-none focus:ring-0 mr-4"
-                                href="#portfolio">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="inline-block mr-1" width="1.5rem"
-                                    height="1.5rem" fill="currentColor" viewBox="0 0 512 512">
-                                    <path
-                                        d="M304,384V360c0-29,31.54-56.43,52-76,28.84-27.57,44-64.61,44-108,0-80-63.73-144-144-144A143.6,143.6,0,0,0,112,176c0,41.84,15.81,81.39,44,108,20.35,19.21,52,46.7,52,76v24"
-                                        style="fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" />
-                                    <line x1="224" y1="480" x2="288" y2="480"
-                                        style="fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" />
-                                    <line x1="208" y1="432" x2="304" y2="432"
-                                        style="fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" />
-                                    <line x1="256" y1="384" x2="256" y2="256"
-                                        style="fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" />
-                                    <path d="M294,240s-21.51,16-38,16-38-16-38-16"
-                                        style="fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px" />
-                                </svg>
-                                Our Work
-                            </a>
                         </div>
-                    </div>
+                    @endif
                 </div>
             </div>
         </div>
@@ -246,7 +250,7 @@
         <!--  End Content -->
 
         <!-- start clients -->
-        @if ($clients !== null)
+        @if ($clients->isNotEmpty())
             <div id="clients" class="section relative py-8 bg-white dark:bg-gray-800">
                 <div class="container xl:max-w-6xl mx-auto px-4">
                     <!-- Heading start -->
@@ -264,7 +268,8 @@
                         </svg>
                         <p class="text-gray-500 leading-relaxed font-light text-xl mx-auto pb-2">We create engaging
                             experiences that are innovatingand beautiful.</p>
-                    </header><!-- End heading -->
+                    </header>
+                    <!-- End heading -->
                 </div>
                 <div class="container xl:max-w-6xl mx-auto px-4">
                     <div class="lex flex-wrap flex-row -mx-4 justify-center">
@@ -295,7 +300,6 @@
                 </div>
             </div>
         @endif
-
         <!-- End brand-->
 
         <!-- Team start -->
@@ -409,11 +413,14 @@
                                     <circle cx="50.1" cy="30.4" r="5" class="stroke-primary"
                                         style="fill: transparent;stroke-width: 2;stroke-miterlimit: 10;"></circle>
                                     <line x1="55.1" y1="30.4" x2="100" y2="30.4"
-                                        class="stroke-primary" style="stroke-width: 2;stroke-miterlimit: 10;"></line>
+                                        class="stroke-primary" style="stroke-width: 2;stroke-miterlimit: 10;">
+                                    </line>
                                     <line x1="45.1" y1="30.4" x2="0" y2="30.4"
-                                        class="stroke-primary" style="stroke-width: 2;stroke-miterlimit: 10;"></line>
+                                        class="stroke-primary" style="stroke-width: 2;stroke-miterlimit: 10;">
+                                    </line>
                                 </svg>
-                                <p class="text-gray-600 leading-relaxed font-light text-xl mx-auto pb-2">Have questions
+                                <p class="text-gray-600 leading-relaxed font-light text-xl mx-auto pb-2">Have
+                                    questions
                                     about service, please contact us.</p>
                             </header>
                             <!-- end section header -->
